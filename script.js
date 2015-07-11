41ac7dd879600f725c5ee62c65e2664e52a2ba1a
robhaj <hajek.rob@gmail.com>
2015-07-11 15:52:20 -0600

    added index.html and script.js to complete chessboard exercise

---
+++ script.js
@@ -0,0 +1,11 @@
+var chess = "";
+var even = "# # # # ";
+var odd = " # # # #";
+
+for ( var i = 0; i < 8; i++ ) {
+
+    if ( i % 2 === 0 ) chess += even + "\n";
+    else chess += odd + "\n";
+}
+
+console.log(chess);
