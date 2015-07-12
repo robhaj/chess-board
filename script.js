4bf858ad9aa8428f959b1a3cdcda8e810704a723
robhaj <hajek.rob@gmail.com>
2015-07-12 10:08:12 -0600

    checked script.js for refactor

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var chess = "";
 var even = "# # # # ";
 var odd = " # # # #";
 
-for ( var i = 0; i < 8; i++ ) {
+for ( var i = 0; i < 9; i++ ) {
 
     if ( i % 2 === 0 ) chess += even + "\n";
     else chess += odd + "\n";
